64c92e5becf743f8efd5de9298ceed8b345c1abd
GeneGulanesJr <ggulanes_180000000405@uic.edu.ph>
2022-03-04 13:43:24 +0800

    a

--- JS/login.js
+++ JS/login.js
@@ -1,5 +1,13 @@
 
-
+// function submitChecker(){
+//
+//    //if validateForm and validateFormPassword return true, then submit
+//    if(validateForm() && validateFormPassword()){
+//       document.getElementById("loginForm").submit();
+//    }else{
+//        return false;
+//    }
+// }
 
 
 
@@ -11,10 +19,13 @@ function validateForm() {
     let x = document.forms["loginForm"]["UserName"].value;
     if ( x.length <= 4) {
         UserName.innerHTML = `<span style="background-color: red">Must Be Equal or Longer than 5 characters</span>`;
-        return true;
+        validateChecker(null);
+
+
 
     }else{
         UserName.innerHTML = `<span style="background-color: green">Valid</span>`;
+        validateChecker(1);
     }
 
 }
@@ -29,9 +40,12 @@ function validateFormPassword(){
     let regex = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)[a-zA-Z\d]{8,}$/;
     //text x if it passes the regex
     if (x.match(regex)) {
-        Password.innerHTML = `<span style="background-color: green">Valid</span>`;}
+        Password.innerHTML = `<span style="background-color: green">Valid</span>`;
+        validateChecker(2);
+    }
     else {
         Password.innerHTML = `<span style="background-color: red">Must Contain at least 1 uppercase, 1 lowercase and 1 number and is 8 characters long</span>`;
+        validateChecker(null);
     }
 
 
